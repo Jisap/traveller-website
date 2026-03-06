1e3a251202c1fd61919b0197a9e859f9f9df0852
Jorge Matos <jisapjisap@gmail.com>
2026-03-06 17:56:02 +0100

    feat: Initialize core UI with global styles, Hero section, and Navbar component.

--- src/Components/Index/Hero/Hero.jsx
+++ src/Components/Index/Hero/Hero.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+
+
+import headerbg from "../../../assets/Index/Hero/slider-bg.jpg"
+import heroimg from "../../../assets/Index/Hero/right-pic.png"
+import plane from "../../../assets/Index/Hero/Plane-With-Line.png"
+import cloud1 from "../../../assets/Index/Hero/Cloud1.png"
+import cloud2 from "../../../assets/Index/Hero/Cloud2.png"
+import { Icon } from "@iconify/react"
+import { Link } from "react-router-dom"
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div
+      className='hero relative bg-cover bg-center min-h-screen xl:min-h-0 overflow-hidden'
+      style={{ backgroundImage: `url(${headerbg})` }}
+    >
+      <div className="bg-overlay mx-auto px-5 sm:px-8 md:px-12 py-16 md:py-25 xl:h-screen flex items-center xl:flex-row flex-col xl:gap-0 gap-20">
+        <div className="hero-content w-full xl:ps-12.5 z-1">
+          <h3 className="text-5xl text-white font-kaushan!">
+            Discover
+          </h3>
+
+          <h1 className="font-kaushan! font-semibold text-[160px] text-white hero-title">
+            The World
+          </h1>
+
+          <div className="mt-5 md:mt-10">
+            <p className="text-white pera-text">
+              The Safety of our customers at all stages
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@
 import Logo from './Logo/Logo'
 import Navmenu from './Navmenu/Navmenu'
 import Toggle from '../Buttons/Toggle'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 const Navbar = () => {
@@ -18,10 +18,30 @@ const Navbar = () => {
     setScrolled(!scrolled)
   }
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
   return (
     <>
-      <div>
-        <div className='flex justify-between relative z-2 w-full h-22 items-center px-4 bg-black'>
+      <div className={`
+        w-full z-20 flex justify-between items-center fixed top-0 left-0 transition-all duration-300 px-[2%] sm:px-[8%]
+        ${scrolled ? "bg-black" : "bg-transparent"}
+        `}
+      >
+        <div className='flex justify-between relative z-2 w-full h-22 items-center px-4'>
           <Logo />
           <Navmenu menuOpen={menuOpen} toggleMenu={toggleMenu} />
           <Toggle toggleMenu={toggleMenu} handleFancyClick={handleFancyClick} />
