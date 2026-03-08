2dce4f59c88da514d4da9f2c0297411a6162e56e
Jorge Matos <jisapjisap@gmail.com>
2026-03-08 15:26:40 +0100

    feat: Implement global styling and the initial About section component.

--- src/Components/Index/About/About.jsx
+++ src/Components/Index/About/About.jsx
@@ -3,12 +3,13 @@
 import icon1 from "../../../assets/Index/About/travel-guide.png"
 import icon2 from "../../../assets/Index/About/mission-icon.png"
 import Mainbtn from "../../Buttons/Mainbtn"
-import authore1 from "../../../assets/Index/About/pic1.jpg"
-import authore2 from "../../../assets/Index/About/pic2.jpg"
-import authore3 from "../../../assets/Index/About/pic3.jpg"
+import author1 from "../../../assets/Index/About/pic1.jpg"
+import author2 from "../../../assets/Index/About/pic2.jpg"
+import author3 from "../../../assets/Index/About/pic3.jpg"
 import airplane from "../../../assets/Index/About/airplane.png"
 import aboutimg1 from "../../../assets/Index/About/about-image01.jpg"
 import aboutimg2 from "../../../assets/Index/About/about-image02.jpg"
+import aboutimg3 from "../../../assets/Index/About/about-image03.jpg"
 
 
 
@@ -27,9 +28,66 @@ const About = () => {
 
         <ul className="space-y-5">
           <li className="flex items-center flex-wrap md:flex-nowrap border border-secondary/30 p-5 gap-5 rounded-xl">
+            <img src={icon1} alt="icon1" className="w-14 h-14" />
 
+            <div>
+              <span className="text-xl font-semibold">Trusted travel guide</span>
+
+              <p>
+                Provides reliable information to help travelers plan their trips efficiently and safely.
+              </p>
+            </div>
+          </li>
+
+          <li className="flex items-center flex-wrap md:flex-nowrap border border-secondary/30 p-5 gap-5 rounded-xl">
+            <img src={icon2} alt="icon2" className="w-14 h-14" />
+
+            <div>
+              <span className="text-xl font-semibold">Mission & Vision</span>
+
+              <p>Aims to connect people to positive experience through travel, helping them see the world differently.
+                Provides reliable information to help travelers plan their trips efficiently and safely.
+              </p>
+            </div>
           </li>
         </ul>
+
+        <div className="flex items-center flex-wrap gap-10 pt-8">
+          <Mainbtn text={"Discover More"} to="/about" />
+
+          <div className="flex items-center gap-5">
+            <div className="author-img flex items-center">
+              <img src={author1} alt="authore1" className="w-10 rounded-full" />
+              <img src={author2} alt="authore2" className="w-10 rounded-full -mx-3" />
+              <img src={author3} alt="authore3" className="w-10 rounded-full " />
+            </div>
+
+            <p className="text-md">
+              <span className="block text-2xl font-bold">
+                3.5k
+              </span>
+              Happy Customer
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="about-image w-full xl:w-[50%] relative hidden xl:flex justify-center items-center h-full">
+        <div className="airplane absolute transition-all duration-500 -top-1 right-0">
+          <img src={airplane} alt="airplane" />
+        </div>
+
+        <div className="about-image1">
+          <img src={aboutimg1} alt="aboutimg1" className="rounded-full w-full" />
+        </div>
+
+        <div className="about-image2">
+          <img src={aboutimg2} alt="aboutimg2" className="h-full w-full" />
+        </div>
+
+        <div className="about-image3">
+          <img src={aboutimg3} alt="aboutimg2" className="h-full w-full" />
+        </div>
       </div>
     </div>
   );
