47cc66df73cec02569b9dda381865361874cd03c
Jorge Matos <jisapjisap@gmail.com>
2026-03-08 16:08:46 +0100

    feat: Add BookingSteps component and integrate it into the Index page.

--- src/Components/Index/Index.jsx
+++ src/Components/Index/Index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from './Hero/Hero'
 import PageTransition from '../Transition/PageTransition'
 import About from './About/About'
+import BookingSteps from './BookingSteps/BookingSteps'
 
 const Index = () => {
   return (
     <PageTransition>
       <Hero />
       <About />
+      <BookingSteps />
     </PageTransition>
   )
 }
