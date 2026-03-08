79fb44441fe67575426e2cd2c29d013066054bc4
Jorge Matos <jisapjisap@gmail.com>
2026-03-08 11:32:08 +0100

    feat: Add new About section component and integrate it into the main Index page.

--- src/Components/Index/Index.jsx
+++ src/Components/Index/Index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './Hero/Hero'
 import PageTransition from '../Transition/PageTransition'
+import About from './About/About'
 
 const Index = () => {
   return (
     <PageTransition>
       <Hero />
+      <About />
     </PageTransition>
   )
 }
