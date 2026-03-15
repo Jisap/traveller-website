b058cfd01255feefabd828af12402976e3d908d2
Jorge Matos <jisapjisap@gmail.com>
2026-03-15 11:38:26 +0100

    feat: create initial Services page with a common banner and service offerings text.

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -13,6 +13,13 @@ const Services = () => {
         bgImage={sectionbanner}
         breadcrumbs={breadcrumbs}
       />
+
+      <div className="absolute right-40 bottom-8 hidden lg:flex flex-col text-white font-kaushan! text-2xl text-end xl:text-5xl z-1">
+        Wonderful Services For You
+        <h2 className="uppercase font-afacad! font-extrabold text-4xl lg:text-6xl xl:text-8xl text-yellow">
+          Services We Offer
+        </h2>
+      </div>
     </>
   )
 }
