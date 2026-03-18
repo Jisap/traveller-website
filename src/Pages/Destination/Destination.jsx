52bb1f83c59799139df611e3c1616d5292d5a32f
Jorge Matos <jisapjisap@gmail.com>
2026-03-18 19:05:54 +0100

    feat: create Destination page to display destination categories with a common banner.

--- src/Pages/Destination/Destination.jsx
+++ src/Pages/Destination/Destination.jsx
@@ -1,5 +1,7 @@
 import sectionbanner from "../../assets/section-banner.jpg"
 import CommonBanner from '../../Components/CommonBanner/CommonBanner';
+import DestinationCtgCard from "../../Components/DestinationCtgCard/DestinationCtgCard";
+import destinations from "../../Data/Destination.json"
 
 
 const breadcrumbs = [
@@ -15,6 +17,12 @@ const Destination = () => {
         title="Destination"
         breadcrumbs={breadcrumbs}
       />
+
+      <div className="destination px-[2%] sm:px-[8%] lg:px-[12%] py-[6%] md:py-[10%] grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-10 bg-[#e6f1f3]">
+        {destinations.map((item) => (
+          <DestinationCtgCard key={item.id} item={item} />
+        ))}
+      </div>
     </>
   )
 }
