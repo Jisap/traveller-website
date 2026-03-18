c17ff60191ced3743ef4b4bda37b247d33bb129d
Jorge Matos <jisapjisap@gmail.com>
2026-03-18 16:36:57 +0100

    feat: Add new Pricing page component with pricing plans and banner.

--- src/Pages/Pricing/Pricing.jsx
+++ src/Pages/Pricing/Pricing.jsx
@@ -113,6 +113,122 @@ const Pricing = () => {
               </div>
             </div>
           </div>
+
+          <div className="tour-price-card relative z-9">
+            <div className="tour-image w-full mb-10">
+              <img src={tour2} alt="tour1" className="bg-center rounded-full w-full" />
+            </div>
+
+            <div className="content">
+              <div className="price-head flex justify-between items-centerw-full pb-5">
+                <h4 className="text-secondary text-3xl font-medium">
+                  Mid-Range-Travel
+                </h4>
+
+                <div className="plan-price text-center">
+                  <span className="block text-4xl font-semibold text-secondary">
+                    $79
+                  </span>
+
+                  <span className="text-secondary">
+                    Per Day
+                  </span>
+                </div>
+              </div>
+
+              <ul className="space-y-4 mb-8">
+                <li className="flex items-center gap-2 text-secondary font-medium">
+                  <Icon icon="fluent:arrow-circle-right-16-regular" width="24" height="24" className="text-yellow" />
+                  3 star hotel (4 nights)
+                </li>
+
+                <li className="flex items-center gap-2 text-secondary font-medium">
+                  <Icon icon="fluent:arrow-circle-right-16-regular" width="24" height="24" className="text-yellow" />
+                  Local taxis
+                </li>
+
+                <li className="flex items-center gap-2 text-secondary font-medium">
+                  <Icon icon="fluent:arrow-circle-right-16-regular" width="24" height="24" className="text-yellow" />
+                  Sightseeing, tickets
+                </li>
+
+                <li className="flex items-center gap-2 text-secondary font-medium">
+                  <Icon icon="fluent:arrow-circle-right-16-regular" width="24" height="24" className="text-yellow" />
+                  Tourist visa
+                </li>
+
+                <li className="flex items-center gap-2 text-secondary font-medium">
+                  <Icon icon="fluent:arrow-circle-right-16-regular" width="24" height="24" className="text-yellow" />
+                  Meal & snacks
+                </li>
+              </ul>
+
+              <div className="flex justify-center items-center">
+                <Mainbtn
+                  to="/contact"
+                  text={"Shop Now"}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="tour-price-card relative z-9">
+            <div className="tour-image w-full mb-10">
+              <img src={tour3} alt="tour1" className="bg-center rounded-full w-full" />
+            </div>
+
+            <div className="content">
+              <div className="price-head flex justify-between items-centerw-full pb-5">
+                <h4 className="text-secondary text-3xl font-medium">
+                  Luxury Travel
+                </h4>
+
+                <div className="plan-price text-center">
+                  <span className="block text-4xl font-semibold text-secondary">
+                    $99
+                  </span>
+
+                  <span className="text-secondary">
+                    Per Day
+                  </span>
+                </div>
+              </div>
+
+              <ul className="space-y-4 mb-8">
+                <li className="flex items-center gap-2 text-secondary font-medium">
+                  <Icon icon="fluent:arrow-circle-right-16-regular" width="24" height="24" className="text-yellow" />
+                  3 star hotel (4 nights)
+                </li>
+
+                <li className="flex items-center gap-2 text-secondary font-medium">
+                  <Icon icon="fluent:arrow-circle-right-16-regular" width="24" height="24" className="text-yellow" />
+                  Local taxis
+                </li>
+
+                <li className="flex items-center gap-2 text-secondary font-medium">
+                  <Icon icon="fluent:arrow-circle-right-16-regular" width="24" height="24" className="text-yellow" />
+                  Sightseeing, tickets
+                </li>
+
+                <li className="flex items-center gap-2 text-secondary font-medium">
+                  <Icon icon="fluent:arrow-circle-right-16-regular" width="24" height="24" className="text-yellow" />
+                  Tourist visa
+                </li>
+
+                <li className="flex items-center gap-2 text-secondary font-medium">
+                  <Icon icon="fluent:arrow-circle-right-16-regular" width="24" height="24" className="text-yellow" />
+                  Meal & snacks
+                </li>
+              </ul>
+
+              <div className="flex justify-center items-center">
+                <Mainbtn
+                  to="/contact"
+                  text={"Shop Now"}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
